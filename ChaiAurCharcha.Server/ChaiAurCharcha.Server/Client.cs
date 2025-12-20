62c96fda03bfd974518555b6ea86e5991d48edf9
rahul <rahulsalinskee@gmail.com>
2025-12-20 15:51:39 +0530

    Successful connection is established between client and server.

---
+++ ChaiAurCharcha.Server/ChaiAurCharcha.Server/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChaiAurCharcha.Server
+{
+    internal class Client
+    {
+        public string UserName { get; set; }
+
+        public Guid UID { get; set; }
+
+        public TcpClient ClientSocket { get; set; }
+
+        public Client(TcpClient clientSocket)
+        {
+            ClientSocket = clientSocket;
+            UID = Guid.NewGuid();
+
+            Console.WriteLine($"{DateTime.Now}: Client is connected with the username {UserName}");
+        }
+    }
+}
--- ChaiAurCharcha.Server/ChaiAurCharcha.Server/Program.cs
+++ ChaiAurCharcha.Server/ChaiAurCharcha.Server/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace ChaiAurCharcha.Server
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ChaiAurCharcha.Server
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static TcpListener _tcpListener;
+
+        internal static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            _tcpListener = new(IPAddress.Parse("127.0.0.1"), 7891);
+
+            _tcpListener.Start();
+
+            var tcpClient = _tcpListener.AcceptTcpClient();
+            Console.WriteLine("Client is connected now!");
         }
     }
 }
